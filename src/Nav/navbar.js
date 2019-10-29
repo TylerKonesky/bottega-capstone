78c74af9d9f7e57c2aa85c52d51e90ef3e293f99
Tyler <tylerkonesky@hotmail.com>
2019-10-29 15:39:57 -0600

    small things again

--- src/Nav/navbar.js
+++ src/Nav/navbar.js
@@ -8,18 +8,18 @@ function NavBar(props) {
     <div className="nav-bar-wrapper">
         <div className="nav-bar-logo">
 
-            <a href="/">Logo</a>
+            <a href="/">Sally's Weird Items</a>
         </div>
         <div className="nav-bar-name">
             {props.page}
         </div>
         <div className="nav-bar-links-wrapper">
-            <div className="nav-bar-link home">
+            {/* <div className="nav-bar-link home">
                 <Link to="/">Home</Link>
             </div>
             <div className="nav-bar-link about">
                 <Link to="/">About</Link>
-            </div>
+            </div> */}
             <div className="nav-bar-link">
                 <Link to="/cart">Cart</Link>
             </div>
